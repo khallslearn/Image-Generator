8ca5a7b98e0e5d992cd363ba30d375a8494e26fe
KH <75588413+khallslearn@users.noreply.github.com>
2022-12-09 19:40:33 -0500

    modified:   index.js 	new file:   public/css/spinner.css 	new file:   public/css/style.css 	new file:   public/index.html 	new file:   public/js/main.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/openai', require('./routes/openaiRoutes'));
