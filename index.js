da3f3e846a99da071315b8c9cbf6d3a16ecb260e
KH <75588413+khallslearn@users.noreply.github.com>
2022-12-09 19:13:04 -0500

    modified:   .env 	new file:   controllers/openaiController.js 	modified:   index.js 	modified:   routes/openaiRoutes.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
- app.use('/openai', require('.routes/openaiRoutes.js'));
+ app.use('/openai', require('./routes/openaiRoutes.js'));
 
+ // Enable body parser
+
+ app.use(express.json());
+ app.use(express.urlencoded({extended: false}));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
