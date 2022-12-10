f1a39c995fe7cc84fb18d56f3d816a3774151f9c
KH <75588413+khallslearn@users.noreply.github.com>
2022-12-09 19:29:36 -0500

    Openai Backend

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
- app.use('/openai', require('./routes/openaiRoutes.js'));
 
- // Enable body parser
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
- app.use(express.json());
- app.use(express.urlencoded({extended: false}));
+// Set static folder
+
+
+app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
